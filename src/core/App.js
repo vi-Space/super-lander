e666b56385961a375d79a7776c1875ef22527d9f
Marko <markoeling@gmail.com>
2018-10-29 01:21:34 +0100

    Added FAQ to Router

--- src/core/App.js
+++ src/core/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import {Home, Impressum, Error} from '../pages/';
-import {Header, Footer} from '../components/';
+import { Home, Impressum, Error } from '../pages/';
+import { Header, Footer } from '../components/';
 
 class App extends Component {
-  
+
 
   render() {
     return (
-        <Router>
-          <React.Fragment>
-            <Header />
-            <Switch>
-              <Route path="/impressum" component={Impressum} />
-              <Route path="/" component={Home} />
-              <Route component={() => <Error title="404" description="This page doesn't exist!" />} />
-            </Switch>
-            <Footer />
-          </React.Fragment>
-        </Router>
+      <Router>
+        <React.Fragment>
+          <Header />
+          <Switch>
+            <Route path="/impressum" component={Impressum} />
+            <Route path="/faq" component={Impressum} />
+            <Route path="/" component={Home} />
+            <Route component={() => <Error title="404" description="This page doesn't exist!" />} />
+          </Switch>
+          <Footer />
+        </React.Fragment>
+      </Router>
     );
   }
 }
